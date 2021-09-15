94e84a47186a6a470a3d8a2c5e148b46a28adb74
George Flevaris <george.flevaris@thinkrit.com>
2021-09-15 10:27:13 +0300

    commited changes

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,9 +7,11 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+
   recipes: Recipe[] = [
     new Recipe('Test recipe', 'This is simply a test recipe', 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/93/Life_In_Recipes.png/640px-Life_In_Recipes.png'),
-    new Recipe('Test recipe', 'This is simply a test recipe', 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/93/Life_In_Recipes.png/640px-Life_In_Recipes.png')
+    new Recipe('Another Test recipe', 'This is simply a test recipe', 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/93/Life_In_Recipes.png/640px-Life_In_Recipes.png')
   ];
 
   constructor() { }
@@ -17,4 +19,7 @@ export class RecipeListComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+  }
 }
