ecdc24c2cdff313ee44f070cf6ba9aa7b072c5c8
George Flevaris <george.flevaris@thinkrit.com>
2021-09-23 18:11:44 +0300

    course complete

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,57 +1,20 @@
 import { NgModule } from "@angular/core";
-import { RouterModule, Routes } from "@angular/router";
-import { AuthComponent } from "./auth/auth.component";
-import { AuthGuard } from "./auth/auth.guard";
-import { RecipeDetailComponent } from "./recipes/recipe-detail/recipe-detail.component";
-import { RecipeEditComponent } from "./recipes/recipe-edit/recipe-edit.component";
-import { RecipeStartComponent } from "./recipes/recipe-start/recipe-start.component";
-import { RecipesResolverService } from "./recipes/recipes-resolver.service";
-import { RecipesComponent } from "./recipes/recipes.component";
-import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
+import { PreloadAllModules, RouterModule, Routes } from "@angular/router";
 
 const appRoutes: Routes = [
-    {
-        path: '',
-        redirectTo: 'recipes',
-        pathMatch: 'full'
-    },
-    {
-        path: 'recipes',
-        component: RecipesComponent,
-        canActivate: [AuthGuard],
-        children: [
-            {
-                path: '',
-                component: RecipeStartComponent
-            },
-            {
-                path: 'new',
-                component: RecipeEditComponent
-            },
-            {
-                path: ':id',
-                component: RecipeDetailComponent,
-                resolve: [RecipesResolverService]
-            }, 
-            {
-                path: ':id/edit',
-                component: RecipeEditComponent,
-                resolve: [RecipesResolverService]
-            }
-        ]
-    },
-    {
-        path: 'shopping-list',
-        component: ShoppingListComponent
-    },
-    {
-        path: 'auth',
-        component: AuthComponent
-    }
+    { path: '', redirectTo: 'recipes', pathMatch: 'full' },
+    //lazy loading
+    //as soon as the user enters /recipes, only then the recipes bundle is downloaded
+    { path: 'recipes', loadChildren: () =>  import('./recipes/recipes.module').then(module => module.RecipesModule)},
+    { path: 'shopping-list', loadChildren: () =>  import('./shopping-list/shopping-list.module').then(module => module.ShoppingListModule)},
+    { path: 'auth', loadChildren: () =>  import('./auth/auth.module').then(module => module.AuthModule)}
 ]
 
 @NgModule({
-    imports: [RouterModule.forRoot(appRoutes)],
+    //preload all modules for example when we are at auth page, download and load all modules
+    //good because we dont have to download all the app when we try to access auth, but later
+    //so the initial download is still small (lazy loading)
+    imports: [RouterModule.forRoot(appRoutes, {preloadingStrategy: PreloadAllModules})],
     exports: [RouterModule]
 })
 export class AppRoutingModule{}
